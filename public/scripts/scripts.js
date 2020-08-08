a38d301b87a6a49485911342a71c591cea979684
AthN3090 <amandevrishi12@gmail.com>
2020-08-08 17:20:25 +0530

    front-end: keywords array added

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
         $(".question-box2").delay(300).fadeIn(250);
         }); 
 });
+function changeState(id){
+    var button = document.getElementById(id);
+    if(button.style.color == "white"){
+        button.style.backgroundColor = "white";
+        button.style.color = "#329643";
+    }else{
+        button.style.backgroundColor = "#329643";
+        button.style.color = "white";
+    }  
+}
+function submitDetails(){
+    var buttons = document.getElementsByClassName("option");
+    Array.from(buttons).forEach(element => {
+        if(element.style.color == "white"){
+            keys.push(element.innerHTML);
+        }
+        
+    });
+}
