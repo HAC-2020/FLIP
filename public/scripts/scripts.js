37a8a99077f02e4ddca1c53858293bd240a5572e
AthN3090 <amandevrishi12@gmail.com>
2020-08-08 13:53:19 +0530

    front-end: second question added

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $(".load-screen").delay(500).fadeOut(500);
-
+    $("#next").click(function(){
+        nickname = $("#nickname").val(); //stores the nickname
+        $(".question-box").fadeOut(250);
+        $(".question-box2").delay(300).fadeIn(250);
+        }); 
 });
