e56f4266bb043992e77c33d98a75da59747df127
AthN3090 <amandevrishi12@gmail.com>
2020-08-09 00:32:27 +0530

    minor changes for hosting on heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 var users = new Array();
 var myId;
 app.use(express.static(__dirname + '/public'));
@@ -69,6 +70,6 @@ io.on('connection', (socket) => {
     })
 });
 
-http.listen(3000, () => {
+http.listen(port, () => {
   console.log('listening on *:3000');
 });
